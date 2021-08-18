07e0f296863fb6458a9c3c417f9b690e313c3a80
Aravindkumar-Palani <palaniaravindkumar@gmail.com>
2021-08-18 23:19:10 +0530

    login page implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Homepage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from "./Login";
+import './App.css';
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <div id="page-wrap">
+                    <Switch>
+                        <Route path="/" exact  ><Login /></Route>
+                        <Route path="/home" ><Home /></Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
